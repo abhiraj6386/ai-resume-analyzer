43bd64311b522a534a4339ff731eeb95586be3a5
abhiraj6386 <tripathi.abhiraj6386@gmail.com>
2026-05-02 00:38:29 +0530

    cors final fix

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "https://ai-resume-analyzer-mu-topaz.vercel.app",
+    origin: true,
     credentials: true
 }))
 
