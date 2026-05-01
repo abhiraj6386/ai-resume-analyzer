3455a9ab546d8c648d17929bb23f75a8a4abfb0c
abhiraj6386 <tripathi.abhiraj6386@gmail.com>
2026-05-02 00:03:31 +0530

    fixed backend url

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://ai-resume-analyzer-wv6i.onrender.com",
     withCredentials: true,
 })
 
